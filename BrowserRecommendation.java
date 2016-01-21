3549416513119634ed9e620796cdf80c25e8d2d4
Christine Callinan <ccallinan16@deerfield.edu>
2016-01-20 20:15:29 -0500

    HTML file for Browser

---
+++ BrowserRecommendation.java
@@ -0,0 +1,23 @@
+import java.applet.Applet;
+import java.awt.HeadlessException;
+
+/**
+ * 
+ */
+
+/**
+ * @author ccallinan16
+ *
+ */
+public class BrowserRecommendation extends Applet
+{
+
+	/**
+	 * @throws HeadlessException
+	 */
+	public BrowserRecommendation() throws HeadlessException
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
